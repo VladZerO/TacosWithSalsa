a9d98e2f1b9ed7c7bac4feffd5b7e8e55e1b247f
VladZerO <inter.arman@gmail.com>
2022-04-30 11:28:10 -0700

    - GridGenerator

--- StarterPack/Assets/Scripts/GridGenerator.cs
+++ StarterPack/Assets/Scripts/GridGenerator.cs
@@ -5,20 +5,55 @@ using UnityEngine;
 public class GridGenerator : MonoBehaviour
 {
 
-    [SerializeField] private GameObject gridPrefab;
+    [SerializeField] private GameObject midTilePrefab;
+    [SerializeField] private GameObject sideTilePrefab;
 
-    [SerializeField] private int amountOfGrids;
-    [SerializeField] private int gridLength;
 
-    private List<GameObject> createdGrids = new List<GameObject>();
+    [SerializeField] private int amountOfRows;
+    [SerializeField] private int tilesPerRow;
+
+    [SerializeField] private int tileWidth;
+    [SerializeField] private int tileLength;
+
+
+
+    private List<GameObject> createdTiles = new List<GameObject>();
+    private List<GameObject> createdRows = new List<GameObject>();
+
+    private GameObject[,] tileGrid;
 
     void Start()
     {
-        for (int x = 0; x < amountOfGrids; x++)
+        tileGrid = new GameObject[amountOfRows, tilesPerRow];
+
+        for (int x = 0; x < amountOfRows; x++)
         {
-            GameObject loGridPrefab = Instantiate(gridPrefab);
-            loGridPrefab.transform.position = new Vector3(0.0f, 0.0f, gridLength * x);
-            createdGrids.Add(loGridPrefab);
+            for (int y = 0; y < tilesPerRow; y++)
+            {
+                GameObject loTilePrefab;
+                
+                if (y == 0)
+                {
+                    loTilePrefab = Instantiate(sideTilePrefab);
+                    loTilePrefab.transform.position = new Vector3(-tileWidth + (y * tileWidth), 0.0f, tileLength * x);
+                    
+                }
+                else if (y == tilesPerRow - 1)
+                {
+                    loTilePrefab = Instantiate(sideTilePrefab);
+                    loTilePrefab.transform.position = new Vector3(-tileWidth + (y * tileWidth), 0.0f, tileLength * x);
+                    loTilePrefab.transform.rotation = Quaternion.Euler(0.0f, 180.0f, 0.0f);
+                }
+                else
+                {
+                    loTilePrefab = Instantiate(midTilePrefab);
+                    loTilePrefab.transform.position = new Vector3(-tileWidth + (y * tileWidth), 0.0f, tileLength * x);
+                }
+
+                createdTiles.Add(loTilePrefab);
+                //<< Getting the coords of each grid.
+                tileGrid[x, y] = loTilePrefab;
+            }
         }
     }
 
