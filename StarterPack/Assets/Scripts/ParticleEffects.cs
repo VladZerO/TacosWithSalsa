4908d06d6ea1902f26660f9dca06d08e784b071d
Varghese Bobus <varghesebobus@gmail.com>
2022-05-01 14:44:58 -0700

    water splash, explosion, running, snow

--- StarterPack/Assets/Scripts/ParticleEffects.cs
+++ StarterPack/Assets/Scripts/ParticleEffects.cs
@@ -5,29 +5,24 @@ using UnityEngine;
 
 public class ParticleEffects : MonoBehaviour
 {
-    public PlayerController playerController;
+   // public PlayerController playerController;
     public bool isGround;
-    public ParticleSystem explosionParticle;
+    public ParticleSystem explosionParticles;
     public ParticleSystem runningParticles;
+    public ParticleSystem splashParticles;
     // Start is called before the first frame update
     void Start()
     {
-        isGround = playerController.isGrounded;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        InWater();
-        runningparticle();
+        runningParticle();
     }
 
-    void InWater()
-    {
-        
-    }
-
-    void runningparticle()
+    void runningParticle()
     {
         if(Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.A)||Input.GetKeyDown(KeyCode.D)||Input.GetKeyDown(KeyCode.S))
             {
@@ -44,7 +39,12 @@ public class ParticleEffects : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Obstacle"))
         {
-            explosionParticle.Play();
+            explosionParticles.Play();
+        }
+        else if(other.gameObject.CompareTag("Water"))
+        {
+            splashParticles.Play();
+
         }
     }
 }
